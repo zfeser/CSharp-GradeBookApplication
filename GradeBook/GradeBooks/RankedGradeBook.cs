95336bb737db6495073bd466acad6b026660ec01
zfeser <60704491+zfeser@users.noreply.github.com>
2020-05-04 09:52:43 -0500

    Update RankedGradeBook.cs
    Updated Type for RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,7 +6,7 @@ namespace GradeBook.GradeBooks
     {
         public RankedGradeBook(string name) : base(name)
         {
-            Type = GradeBookType.Standard;
+            Type = GradeBookType.Ranked;
         }
 }
 }
