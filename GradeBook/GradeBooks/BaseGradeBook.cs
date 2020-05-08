64a4564ab76d9d15a8d016ea23a7065421be97a4
zfeser <60704491+zfeser@users.noreply.github.com>
2020-05-08 10:43:03 -0500

    Update BaseGradeBook.cs

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -9,7 +9,7 @@ using Newtonsoft.Json.Linq;
 
 namespace GradeBook.GradeBooks
 {
-    internal abstract class BaseGradeBook
+    public abstract class BaseGradeBook
     {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
