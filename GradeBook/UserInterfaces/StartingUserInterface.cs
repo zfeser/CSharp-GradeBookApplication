57bca6e1be3268f9ade9c43b13f3d058cb43854b
zfeser <60704491+zfeser@users.noreply.github.com>
2020-05-08 10:31:59 -0500

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -39,8 +39,18 @@ namespace GradeBook.UserInterfaces
                 Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
-            var name = parts[3];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            var name = parts[1];
+            var type = parts[2].ToLower();
+            BaseGradeBook gradeBook;
+            if (type == "standard")
+                gradeBook = new StandardGradeBook(name);
+            else if (type == "ranked")
+                gradeBook = new RankedGradeBook(name);
+            else
+            {
+                Console.WriteLine("{0} is not a supported type of gradebook. please try again.", type);
+                return
+            }
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
