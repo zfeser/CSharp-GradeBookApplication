c26df80cab4c63021691b1a5d2c790f6b057a46d
zfeser <60704491+zfeser@users.noreply.github.com>
2020-05-08 10:17:07 -0500

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -37,5 +37,14 @@ namespace GradeBook.GradeBooks
             }
             base.CalculateStatistics();
         }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
