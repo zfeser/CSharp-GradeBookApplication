033fb1f0f36d947c77260807f7c23808e8b09929
zfeser <60704491+zfeser@users.noreply.github.com>
2020-05-08 10:33:03 -0500

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -49,7 +49,7 @@ namespace GradeBook.UserInterfaces
             else
             {
                 Console.WriteLine("{0} is not a supported type of gradebook. please try again.", type);
-                return
+                return;
             }
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
